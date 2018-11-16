fa19bcad205763dc6742038cba29a88013d86316
schoud27 <300990285@cencol.ca>
2018-11-15 19:44:46 -0500

    Buy Ticket Screen Created

--- app/src/main/java/com/example/a300985590/ttcpay/BuyTicketActivity.java
+++ app/src/main/java/com/example/a300985590/ttcpay/BuyTicketActivity.java
@@ -8,19 +8,63 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 public class BuyTicketActivity extends AppCompatActivity {
 
+    private EditText adultCountDisplay;
+    private LinearLayout shoppingLayout;
+    private static int  adultCount=0, seniorCount=0, studentCount=0;
+    private Button cancelBtn, proceedBtn;
 
+    private EditText seniorCountDisplay;
+
+    private HashMap<String,Integer> adultTicketMap=null;
+    private HashMap<String,Integer> seniorTicketMap=null;
+    private HashMap<String,Integer> studentTicketMap=null;
+
+
+    private static int adultQuantityCount=0;
+    private static int studentQuantityCount=0;
+    private static int seniorQuantityCount=0;
+
+
+    //Menu Attributes starts
     private DrawerLayout dl;
     private ActionBarDrawerToggle t;
     private NavigationView nv;
+    //Menu Attributes ends
+
+    HashMap<String,Integer> adultMap;
+    HashMap<String,Integer> seniorMap;
+    HashMap<String,Integer> studentMap;
+
+    double adultPrice=3.25;
+    double seniorPrice=2.0;
+    double studentPrice=2.10;
+    private Intent intent;
+    private EditText studentCountDisplay;
+
+    TextView adultCatName, seniorCatName, studentCatName,totalField,totalFieldPrice;
+    TextView adultcategoryQuantity, seniorcategoryQuantity, studentcategoryQuantity;
+    TextView adultcategoryPrice, seniorategoryPrice, studentcategoryPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_ticket);
+        adultTicketMap=new HashMap<String,Integer>();
+        seniorTicketMap=new HashMap<String,Integer>();
+        studentTicketMap=new HashMap<String,Integer>();
 
+        //Menu Code starts
         dl = (DrawerLayout)findViewById(R.id.activity_buy_ticket);
         t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
 
@@ -28,7 +72,6 @@ public class BuyTicketActivity extends AppCompatActivity {
         t.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         nv = (NavigationView)findViewById(R.id.nv);
         nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -74,9 +117,51 @@ public class BuyTicketActivity extends AppCompatActivity {
 
             }
         });
+        //Menu code ends
+
+
+
+        adultCountDisplay=(EditText)findViewById(R.id.adultCount);
+        seniorCountDisplay= (EditText)findViewById(R.id.seniorCount);
+        studentCountDisplay = (EditText)findViewById(R.id.studentCount);
+
+
+        shoppingLayout=(LinearLayout)findViewById(R.id.shoppingCartLayout);
+
+        cancelBtn=(Button)findViewById(R.id.cancelBtn);
+        proceedBtn=(Button)findViewById(R.id.proceedBtn);
+
+        adultCount=0;
+        seniorCount=0;
+        studentCount=0;
+
+        adultMap=new HashMap<String,Integer>();
+        seniorMap=new HashMap<String,Integer>();
+        studentMap=new HashMap<String,Integer>();
+
+
+        TextView mtextView=(TextView)findViewById(R.id.adultPrice);
+        mtextView.setText("  $"+Double.toString(adultPrice));
+
+        TextView wtextView=(TextView)findViewById(R.id.seniorPrice);
+        wtextView.setText("  $"+Double.toString(seniorPrice));
+
+        TextView dtextView=(TextView)findViewById(R.id.studentPrice);
+        dtextView.setText("  $"+Double.toString(studentPrice));
+
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                intent=new Intent(getApplicationContext(),BuyPassActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 
+    //Required  Method for Menu
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -84,5 +169,270 @@ public class BuyTicketActivity extends AppCompatActivity {
             return true;
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void  plusClick(View view)
+    {
+
+        switch(view.getId())
+        {
+            case R.id.adultPlus:
+                adultQuantityCount=adultQuantityCount+1;
+                if(adultQuantityCount>0 && adultQuantityCount<6) {
+                    adultCountDisplay.setText(adultQuantityCount + "");
+                    adultMap.put("monthlyPass", adultQuantityCount);
+                }
+                else
+                {
+                    adultQuantityCount=adultQuantityCount-1;
+                    Toast.makeText(this,"You can only purchase maximum of 5 tickets of same category.",Toast.LENGTH_SHORT).show();
+
+                }
+                break;
+            case R.id.seniorPlus:
+                seniorQuantityCount=seniorQuantityCount+1;
+                if(seniorQuantityCount>0 && seniorQuantityCount<6) {
+
+                    seniorCountDisplay.setText(seniorQuantityCount + "");
+                    seniorMap.put("dailyPass", seniorQuantityCount);
+                }
+                else
+                {
+                    seniorQuantityCount=seniorQuantityCount-1;
+                    Toast.makeText(this,"You can only purchase maximum of 5 tickets of same category.",Toast.LENGTH_SHORT).show();
+
+                }
+                break;
+            case R.id.studentPlus:
+                studentQuantityCount=studentQuantityCount+1;
+                if(studentQuantityCount>0 && studentQuantityCount<6) {
+
+                    studentCountDisplay.setText(studentQuantityCount + "");
+                    studentMap.put("weeklyPass", studentQuantityCount);
+                }
+                else
+                {
+                    studentQuantityCount=studentQuantityCount-1;
+                    Toast.makeText(this,"You can only purchase maximum of 5 tickets of same category.",Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+        }
+        addToCart();
+    }
+    public void  minusClick(View view)
+    {
+        try {
+            switch (view.getId()) {
+                case R.id.adultMinus:
+                    adultQuantityCount = adultQuantityCount - 1;
+                    if (adultQuantityCount >= 0) {
+                        adultCountDisplay.setText(adultQuantityCount + "");
+                        adultMap.put("monthlyPass", adultQuantityCount);
+                    } else {
+                        adultQuantityCount = adultQuantityCount + 1;
+                        Toast.makeText(this, "Quantity can not be less than 0.", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                case R.id.seniorMinus:
+                    seniorQuantityCount = seniorQuantityCount - 1;
+                    if (seniorQuantityCount >= 0) {
+                        seniorCountDisplay.setText(seniorQuantityCount + "");
+                        seniorMap.put("dailyPass", seniorQuantityCount);
+                    } else {
+                        seniorQuantityCount = seniorQuantityCount + 1;
+                        Toast.makeText(this, "Quantity can not be less than 0.", Toast.LENGTH_SHORT).show();
+
+                    }
+                    break;
+                case R.id.studentMinus:
+                    studentQuantityCount = studentQuantityCount - 1;
+                    if (studentQuantityCount >= 0) {
+                        studentCountDisplay.setText(studentQuantityCount + "");
+                        studentMap.put("weeklyPass", studentQuantityCount);
+                    } else {
+                        studentQuantityCount = studentQuantityCount + 1;
+                        Toast.makeText(this, "Quantity can not be less than 0.", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+
+            }
+            addToCart();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public void addToCart()
+    {
+        shoppingLayout.removeAllViews();
+        if(adultQuantityCount>0)
+        {
+            try
+            {
+                LinearLayout ll=new LinearLayout(this);
+                ll.setOrientation(LinearLayout.HORIZONTAL);
+                ll.setId(R.id.layout1);
+                ll.setPadding(5,20,0,5);
+
+
+                adultCatName=new TextView(this);
+                adultCatName.setWidth(400);
+                adultCatName.setText("ADULT");
+                adultCatName.setTextColor(getResources().getColor(R.color.black));
+                adultCatName.setTextSize(18);
+                ll.addView(adultCatName);
+
+                adultcategoryQuantity=new TextView(this);
+                adultcategoryQuantity.setWidth(400);
+                adultcategoryQuantity.setText(adultQuantityCount+"");
+                adultcategoryQuantity.setTextColor(getResources().getColor(R.color.black));
+                adultcategoryQuantity.setTextSize(18);
+                ll.addView(adultcategoryQuantity);
+
+                double totalPrice=adultQuantityCount*adultPrice;
+
+                adultcategoryPrice=new TextView(this);
+                adultcategoryPrice.setWidth(200);
+                adultcategoryPrice.setText("$"+totalPrice);
+                adultcategoryPrice.setTextColor(getResources().getColor(R.color.black));
+                adultcategoryPrice.setTextSize(18);
+                ll.addView(adultcategoryPrice);
+
+                shoppingLayout.addView(ll);
+
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+                System.out.print("Inside prepare cart exception is : "+e.getMessage());
+            }
+
+        }
+
+        if(seniorQuantityCount>0)
+        {
+            try
+            {
+                LinearLayout ll=new LinearLayout(this);
+                ll.setOrientation(LinearLayout.HORIZONTAL);
+                ll.setPadding(5,40,0,5);
+
+                seniorCatName=new TextView(this);
+                seniorCatName.setWidth(400);
+                seniorCatName.setText("SENIOR");
+                seniorCatName.setTextColor(getResources().getColor(R.color.black));
+                seniorCatName.setTextSize(18);
+                ll.addView(seniorCatName);
+
+                seniorcategoryQuantity=new TextView(this);
+                seniorcategoryQuantity.setWidth(400);
+                seniorcategoryQuantity.setText(seniorQuantityCount+"");
+                seniorcategoryQuantity.setTextColor(getResources().getColor(R.color.black));
+                seniorcategoryQuantity.setTextSize(18);
+                ll.addView(seniorcategoryQuantity);
+
+                double totalPrice=seniorQuantityCount*seniorPrice;
+
+                seniorategoryPrice=new TextView(this);
+                seniorategoryPrice.setWidth(200);
+                seniorategoryPrice.setText("$"+totalPrice);
+                seniorategoryPrice.setTextColor(getResources().getColor(R.color.black));
+                seniorategoryPrice.setTextSize(18);
+                ll.addView(seniorategoryPrice);
+
+                shoppingLayout.addView(ll);
+
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+                System.out.print("Inside prepare cart exception is : "+e.getMessage());
+            }
+
+        }
+        if(studentQuantityCount>0)
+        {
+            try
+            {
+                LinearLayout ll=new LinearLayout(this);
+                ll.setOrientation(LinearLayout.HORIZONTAL);
+                ll.setPadding(5,40,0,5);
+
+                studentCatName=new TextView(this);
+                studentCatName.setTextColor(getResources().getColor(R.color.black));
+                studentCatName.setTextSize(18);
+                studentCatName.setWidth(400);
+                studentCatName.setText("STUDENT");
+                ll.addView(studentCatName);
+
+                studentcategoryQuantity=new TextView(this);
+                studentcategoryQuantity.setWidth(400);
+                studentcategoryQuantity.setText(studentQuantityCount+"");
+                studentcategoryQuantity.setTextColor(getResources().getColor(R.color.black));
+                studentcategoryQuantity.setTextSize(18);
+                ll.addView(studentcategoryQuantity);
+
+                double totalPrice=studentQuantityCount*studentPrice;
+
+               studentcategoryPrice=new TextView(this);
+                studentcategoryPrice.setWidth(200);
+                studentcategoryPrice.setTextColor(getResources().getColor(R.color.black));
+                studentcategoryPrice.setTextSize(18);
+                studentcategoryPrice.setText("$"+totalPrice);
+                ll.addView(studentcategoryPrice);
+
+                shoppingLayout.addView(ll);
+
+
+
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+                System.out.print("Inside prepare cart exception is : "+e.getMessage());
+            }
+
+        }
+
+
+        if(adultQuantityCount>0 || seniorQuantityCount>0 || studentQuantityCount>0)
+        {
+
+
+            float totalPrice=Float.parseFloat( (adultQuantityCount*adultPrice + seniorQuantityCount*seniorPrice + studentQuantityCount*studentPrice)+"");
+
+            LinearLayout ll=new LinearLayout(this);
+            ll.setOrientation(LinearLayout.HORIZONTAL);
+            ll.setPadding(5,40,0,5);
+            totalField=new TextView(this);
+            totalField.setTextColor(getResources().getColor(R.color.black));
+            totalField.setTextSize(24);
+            totalField.setWidth(400);
+            totalField.setText("Total Price : ");
+            ll.addView(totalField);
+
+
+            totalFieldPrice=new TextView(this);
+            totalFieldPrice.setTextColor(getResources().getColor(R.color.black));
+            totalFieldPrice.setTextSize(24);
+            totalFieldPrice.setWidth(400);
+            totalFieldPrice.setText("$"+totalPrice);
+            ll.addView(totalFieldPrice);
+
+
+            shoppingLayout.addView(ll);
+
+
+            proceedBtn.setVisibility(View.VISIBLE);
+            cancelBtn.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            proceedBtn.setVisibility(View.INVISIBLE);
+            cancelBtn.setVisibility(View.INVISIBLE);
+        }
     }
 }
